d237444560414c369d90c5cb1ae64669e7b55ac0
Himangshu1086 <baishyahimangshu499@gmail.com>
2020-08-31 11:03:14 +0530

    auth

--- blog/views.py
+++ blog/views.py
@@ -3,12 +3,13 @@ from django.utils import timezone
 from  .models import Post
 from .forms import PostForm
 from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render (request,'blog/post_list.html' , {'posts': posts})
         
-
+@login_required
 def post_new(request):
     if request.method =="POST":
         form = PostForm(request.POST)
